0ca1e2116178771e6b17aa0c8074a6a7615ab4a6
badr ghanbi <bghanbi50@gmail.com>
2022-10-31 14:13:47 +0100

    set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const Person=()=>{
+  return (
+    <>
+    <h2>Name: Nyc {2025-1}</h2>
+    <h3>Last:is my dream</h3>
+  
+    </>
+  )
+}
+const App= () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person/>
     </div>
+    
   );
 }
 
