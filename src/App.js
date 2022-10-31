9e0fef900a2a641e92c1156d9541a17651eaeb30
badr ghanbi <bghanbi50@gmail.com>
2022-10-31 16:12:37 +0100

    adding the svg search icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import {useEffect} from 'react';
 
+const API_URL =    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 
 const App=() => {
+const searchMovis=async (title) => {
+    let response=await fetch(`${API_URL}${title}`);
+    let data=await response.json();
+    console.log(data)
+}  
+
+useEffect(()=>{
+    searchMovis('saw');
+},[])
+
     return (
         <h1>hello world</h1>
     )
